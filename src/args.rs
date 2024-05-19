befd658dba7f72711ca3fa780bf6e4a2e1e9407d
Skypex <84551201+Skyppex@users.noreply.github.com>
2024-05-19 22:17:14 +0200

    add int, float, uuid, and url generation

--- src/args.rs
+++ src/args.rs
@@ -1,14 +1,15 @@
-use clap::{ArgGroup, Parser};
+use std::str::FromStr;
+
+use clap::{ArgGroup, Error, Parser, Subcommand};
 
 /// Write a concise description of the command here.
 #[derive(Debug, Clone, Parser)]
 #[command(version, author, about)]
 #[command(group=ArgGroup::new("log").args(["verbose", "quiet"]).multiple(false))]
-pub struct Args {
-    /// The source file to read from. If not provided, read from stdin.
-    #[arg(short, long)]
-    pub source: Option<String>,
-
+pub struct GenArgs {
+    // /// The source file to read from. If not provided, read from stdin.
+    // #[arg(short, long)]
+    // pub source: Option<String>,
     /// The destination file to write to. If not provided, write to stdout.
     #[arg(short, long)]
     pub destination: Option<String>,
@@ -21,4 +22,109 @@ pub struct Args {
     /// Errors will still be printed to stderr.
     #[arg(short, long)]
     pub quiet: bool,
+
+    #[command(subcommand)]
+    pub commands: Command,
+}
+
+#[derive(Subcommand, Debug, Clone)]
+pub enum Command {
+    /// Generate a random integer.
+    /// Use conventional range notation (e.g. 1..100).
+    /// The range is inclusive.
+    /// If no range is specified, the default is 0..100.
+    Int {
+        range: Option<IntRange>,
+    },
+    /// Generate a random floating-point number.
+    /// Use conventional range notation (e.g. 1.0..100.0).
+    /// The range is inclusive.
+    /// If no range is specified, the default is 0.0..1.0.
+    Float {
+        range: Option<FloatRange>,
+    },
+    /// Generate a random UUID.
+    /// Optionally specify the version.
+    /// If not specified, version 4 is used.
+    Uuid {
+        version: Option<UuidVersion>,
+    },
+    /// Generate a random URL.
+    /// Optionally specify the length of the generated strings
+    /// and the number of path segments.
+    Url {
+        length: Option<usize>,
+
+        #[arg(short, long)]
+        path: Option<Option<u8>>,
+
+        #[arg(short, long)]
+        query: bool,
+    },
+}
+
+#[derive(Debug, Clone, Parser)]
+pub struct IntRange {
+    pub min: i32,
+    pub max: i32,
+}
+
+impl FromStr for IntRange {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let parts: Vec<&str> = s.split("..").collect();
+
+        if parts.len() != 2 {
+            return Err(Error::new(clap::error::ErrorKind::ValueValidation));
+        }
+
+        let min = parts[0].parse().map_err(|_| Error::new(clap::error::ErrorKind::ValueValidation))?;
+        let max = parts[1].parse().map_err(|_| Error::new(clap::error::ErrorKind::ValueValidation))?;
+
+        Ok(IntRange { min, max })
+    }
+}
+
+#[derive(Debug, Clone, Parser)]
+pub struct FloatRange {
+    pub min: f32,
+    pub max: f32,
+}
+
+impl FromStr for FloatRange {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let parts: Vec<&str> = s.split("..").collect();
+
+        if parts.len() != 2 {
+            return Err(Error::new(clap::error::ErrorKind::ValueValidation));
+        }
+
+        let min = parts[0].parse().map_err(|_| Error::new(clap::error::ErrorKind::ValueValidation))?;
+        let max = parts[1].parse().map_err(|_| Error::new(clap::error::ErrorKind::ValueValidation))?;
+
+        Ok(FloatRange { min, max })
+    }
+}
+
+#[derive(Debug, Clone, Parser,)]
+pub enum UuidVersion {
+    Empty,
+    V4,
+    Max,
+}
+
+impl FromStr for UuidVersion {
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "empty" => Ok(UuidVersion::Empty),
+            "4" | "v4" | "ver4" | "version4" => Ok(UuidVersion::V4),
+            "max" => Ok(UuidVersion::Max),
+            _ => Err(Error::new(clap::error::ErrorKind::ValueValidation)),
+        }
+    }
 }
--- src/program.rs
+++ src/program.rs
@@ -1,10 +1,80 @@
-use crate::args::Args;
+use crate::args::{Command, FloatRange, GenArgs, IntRange, UuidVersion};
+use rand::Rng;
+use random_string::{charsets, generate, generate_rng};
+use uuid::Uuid;
 
-pub fn run(input: String, args: Args) -> String {
-    todo!("Write the program logic here");
+pub fn run(args: GenArgs) -> String {
+    return match args.commands {
+        Command::Int { range } => generate_int(range),
+        Command::Float { range } => generate_float(range),
+        Command::Uuid { version } => generate_uuid(version),
+        Command::Url { 
+            length,
+            path,
+            query,
+        } => generate_url(
+            length,
+            path,
+            query
+        ),
+    };
+}
+
+fn generate_int(range: Option<IntRange>) -> String {
+    let mut rng = rand::thread_rng();
+    let min = range.clone().map_or(0, |r| r.min);
+    let max = range.map_or(100, |r| r.max);
+    rng.gen_range(min..=max).to_string()
+}
+
+fn generate_float(range: Option<FloatRange>) -> String {
+    let mut rng = rand::thread_rng();
+    let min = range.clone().map_or(0.0, |r| r.min);
+    let max = range.map_or(1.0, |r| r.max);
+    rng.gen_range(min..=max).to_string()
+}
+
+fn generate_uuid(version: Option<UuidVersion>) -> String {
+    return match version {
+        Some(UuidVersion::Empty) => Uuid::nil().to_string(),
+        Some(UuidVersion::Max) => Uuid::max().to_string(),
+        Some(UuidVersion::V4) | None => Uuid::new_v4().to_string(),
+    }
+}
+
+fn generate_url(
+    length: Option<usize>,
+    path: Option<Option<u8>>,
+    query: bool) -> String {
+    
+    let protocol = "https".to_owned();
+    let domain = gen_str(length) + "." + &gen_str(Some(3));
+    let paths = if let Some(p) = path {
+        let mut paths = Vec::new();
+
+        for _ in 0..(p.unwrap_or(1)) {
+            paths.push(gen_str(length));
+        }
+
+        Some(paths)
+    } else {
+        None
+    };
+
+    match (paths, query) {
+        (None, false) => return format!("{}://{}", protocol, domain),
+        (Some(p), false) => return format!("{}://{}/{}", protocol, domain, p.join("/")),
+        (None, true) => return format!("{}://{}/?{}", protocol, domain, gen_str(length)),
+        (Some(p), true) => return format!("{}://{}/{}?{}", protocol, domain, p.join("/"), gen_str(length)),
+    }
+}
+
+fn gen_str(length: Option<usize>) -> String {
+    match length {
+        Some(l) => generate(l, charsets::ALPHA_LOWER),
+        None => generate_rng(5..15, charsets::ALPHA_LOWER),
+    }
 }
 
 #[cfg(test)]
-mod tests {
-    
-}
+mod tests {}
